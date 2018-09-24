6053ec1f5a2d19a4bbe91a489dedd175e7960d97
David Imhoff <dimhoff.devel@gmail.com>
2018-09-24 11:35:37 +0200

    Update copyright year

--- src/ring_buf.h
+++ src/ring_buf.h
@@ -1,7 +1,7 @@
 /**
  * ring_buf.h - Byte based ring buffer
  *
- * Copyright (c) 2015, David Imhoff <dimhoff.devel@gmail.com>
+ * Copyright (c) 2018, David Imhoff <dimhoff.devel@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
